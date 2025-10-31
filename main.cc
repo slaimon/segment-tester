7ab5ed034fc2594a548485f04507d3c44f27e564
Slaimon <simo.bertolucci@gmail.com>
2025-10-31 11:15:03 +0100

    Edit main.cc to accept arbitrary filename

--- main.cc
+++ main.cc
@@ -6,14 +6,21 @@
 #include <string>
 using namespace std;
 /*
-Test the values in err.csv on the for-loop version
-of Segmento to see if they still cause a problem 
+  Test the values in err.csv on the for-loop version
+  of Segmento to see if they still cause a problem 
 */
 
-unsigned int width = 1280;
-unsigned int height = 800;
+unsigned int width;
+unsigned int height;
 
-#include "forloop.cc"
+// ugly expand-and-stringize trick
+#define STRR(x) #x
+#define STR(x) STRR(x)
+#ifdef FILENAME
+#include STR(FILENAME)
+#else
+#error "Please define FILENAME with the path to the Segmento function you want tested."
+#endif
 
 inline void log(unsigned int x, unsigned int y,
                 unsigned int x2, unsigned int y2)
@@ -48,18 +55,19 @@ void run(vector<unsigned int> args) {
 
 int main(int argc, char** argv) {
     if (argc != 4) {
-        cout << "Usage: " << argv[0] << " <WIDTH> <HEIGHT> <INPUT_FILE>" << endl;
-        cout << "Reads inputs from a file and tests them on the for-loop version of Segmento, "
-            << "using a canvas of the given width and height." << endl;
+        cout << "Usage: " << argv[0] << " <INPUT_FILE> <WIDTH> <HEIGHT>" << endl;
+        cout << "Reads inputs from a file and tests them on the " << STR(FILENAME)
+            << " version of Segmento, using a canvas of the given width and height." << endl;
+        
+        return -1;
     }
-    width = stoul(argv[1]);
-    height = stoul(argv[2]);
-    vector<vector<unsigned int>> tests = readCSV(argv[3]);
+    vector<vector<unsigned int>> tests = readCSV(argv[1]);
+    width = stoul(argv[2]);
+    height = stoul(argv[3]);
 
     for(int i = 0; i < tests.size(); i++) {
         run(tests[i]);
     }
-    cout << "success!" << endl;
 
     return 0;
 }
